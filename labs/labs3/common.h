26bd64536369010dfe7e1a3bde2aefb5179ad9e0
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2024-09-17 15:54:25 +0200

    Added labs3 and all the exercises.

---
+++ labs/labs3/common.h
@@ -0,0 +1,9 @@
+// le fichier en-tête common.h
+#ifndef __common_h__
+#define __common_h__
+
+// déclaration des fonctions
+extern double GetTime();       // protoype de GetTime
+extern void Spin(int howlong); // prototype de Spin
+                               //
+#endif
