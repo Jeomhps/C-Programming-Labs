81c5cd6e96b8cc7aa574b26e9a818d4c329721f8
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2024-09-24 21:21:33 +0200

    Added exo2 of labs4 and Makefile

--- labs/labs4/exo2/exo_2.c
+++ labs/labs4/exo2/exo_2.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void toto(int *z) {
-  /* Here in this function I pass the address of an integer to modify the
-   * integer using a pointer to point where the integer is stored. */
-  *z = 40;
-  printf("After modification z is equal to %d in the function toto\n", *z);
+int toto(int z) {
+  /* Here in this function I pass an integer I modify it and I
+   * return the value and assign it back to the integer itself. */
+  z = 40;
+  printf("After modification z is equal to %d in the function toto\n", z);
+  return z;
 }
 
 int main(int argc, char *argv[]) {
   int z = 10;
-  toto(&z);
+  z = toto(z);
   printf("In the main function z is equal to %d after the modifiction in "
          "the funciton toto\n",
          z);
